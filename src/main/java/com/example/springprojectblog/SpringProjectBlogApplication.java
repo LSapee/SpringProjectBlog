5be0d27598251433720a6a72d3770aa7e15a30cf
Lsapee <dlworhks94@gmail.com>
2022-03-01 02:04:29 +0900

    Initial commit

---
+++ src/main/java/com/example/springprojectblog/SpringProjectBlogApplication.java
@@ -0,0 +1,13 @@
+package com.example.springprojectblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringProjectBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springprojectblog/SpringProjectBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springprojectblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
