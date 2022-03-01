051ce77abf78f7f7d0a8add8bdfd39af65b9cfb8
Lsapee <dlworhks94@gmail.com>
2022-03-02 01:59:43 +0900

    gitignore test

--- src/main/java/com/example/springprojectblog/SpringProjectBlogApplication.java
+++ src/main/java/com/example/springprojectblog/SpringProjectBlogApplication.java
@@ -9,5 +9,7 @@ public class SpringProjectBlogApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringProjectBlogApplication.class, args);
     }
-
+    
+    
+//    혹시 8080포트 실행중이라고 메시지가 나오지만 cmd 에서 pid 8080 포트가 안보일떄 해결방안 찾아보기 컴퓨터 재부팅 이외의
 }
