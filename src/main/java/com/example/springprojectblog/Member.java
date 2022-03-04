12d12b0b7e59d89346e52f090bb88bdac4e5456a
Lsapee <dlworhks94@gmail.com>
2022-03-05 01:51:27 +0900

    builder Test

--- src/main/java/com/example/springprojectblog/Member.java
+++ src/main/java/com/example/springprojectblog/Member.java
@@ -1,48 +1,32 @@
 package com.example.springprojectblog;
 
+import lombok.*;
+
+//@Getter
+//@Setter
+@Data //Getter + Setter
+//@AllArgsConstructor // 전체 생성자
+@NoArgsConstructor // 빈 생성자
+//@RequiredArgsConstructor // final 붙은 애들에 대한 Constructor이 붙음
 public class Member {
     private int id;
     private String username;
     private String password;
     private String email;
 
+    @Builder
     public Member(int id, String username, String password, String email) {
         this.id = id;
         this.username = username;
         this.password = password;
         this.email = email;
     }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
+/*  builder가 없으면 생성자를 하나 더 만들어서 처리해야함
+    public Member( String username, String password, String email) {
         this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
         this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
         this.email = email;
     }
+*/
 
 }
--- src/main/java/com/example/springprojectblog/controller/HttpControllerTest.java
+++ src/main/java/com/example/springprojectblog/controller/HttpControllerTest.java
@@ -9,10 +9,27 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class HttpControllerTest {
 
+    private static final String TAG = "HttpControllerTest : ";
+
+    @GetMapping("http/lombok")
+    public String lombokTest(){
+//      Member m = new Member(1,"asd","dsa","email");
+//      builder 사용시
+//      내가 원하는 것만 가져오기
+//      builder 추가 장점 순서대로 작성에서 실수가 생길 것을 예방한다.
+        Member m = Member.builder().password("1234").email("dsa@dsa.com").username("asda").build();
+        System.out.println(TAG+"getter : " +m.getId());
+        m.setId(5000);
+        System.out.println(TAG+"getter : " +m.getId());
+        return "lombok test 완료";
+    }
+
 //    인터넷 브라우저 요청은 무조껀 get 요청밖에 할 수 없다.
     @GetMapping("/http/get")
 // get 방식으로 요청 - 쿼리스트링
     public String getTest(Member m) {
+        System.out.println(TAG + "");
+
         return "get 요청" + m.getId() + m.getUsername() + m.getPassword() + m.getEmail();
     }
 
