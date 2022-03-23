898c20133aafb30abfce46f39169c2b8e98c03ba
Lsapee <dlworhks94@gmail.com>
2022-03-23 23:25:23 +0900

    kakao token setting

---
+++ src/main/java/com/example/springprojectblog/model/KakaoProfile.java
@@ -0,0 +1,18 @@
+package com.example.springprojectblog.model;
+
+import javax.annotation.Generated;
+
+@Generated("jsonschema2pojo")
+public class KakaoProfile {
+
+    private Object nickname;
+
+    public Object getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(Object nickname) {
+        this.nickname = nickname;
+    }
+
+}
